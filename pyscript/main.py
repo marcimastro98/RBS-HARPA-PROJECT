d086d09627f0020875b28fdf5a63f66fc4c91756
Marcello <82907329+marcimastro98@users.noreply.github.com>
2024-02-18 13:33:58 +0100

    aggiunta funzionalità di aggiornamento nel caso di nuovi dati nei csv, cache e log

--- pyscript/main.py
+++
@@ -1,8 +0,0 @@
-from clean_csv import clean
-from db_conn.db_conn import db_conn
-from log_function import create_log
-
-if __name__ == '__main__':
-    log = create_log()
-    tables, cur, conn = db_conn(log)
-    clean(log, cur, conn)
